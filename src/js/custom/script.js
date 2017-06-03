32e2eafae4024081fad366839a1a21017a7e06d1
Ciprian Turcu <turcuciprian1@gmail.com>
2017-06-03 16:31:43 +0300

    added basic module and controller

--- src/js/custom/script.js
+++ src/js/custom/script.js
@@ -1,3 +1,11 @@
-(function(){
+(function() {
+        console.log('asd');
+        //http://localhost/wordpress/wp-json/rest-basic/v1
+        var app = angular.module("ngTests", []);
+        console.log('asd2');
 
-});
+        app.controller('MainController', ['$http', function($http) {
+          console.log('inside controller');
+
+        }]);
+      })();

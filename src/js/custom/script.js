2bd67070fe43c6762d4126d01155e34bfda16301
Ciprian Turcu <turcuciprian1@gmail.com>
2017-06-03 16:33:27 +0300

    removed console logs

--- src/js/custom/script.js
+++ src/js/custom/script.js
@@ -1,11 +1,8 @@
 (function() {
-        console.log('asd');
         //http://localhost/wordpress/wp-json/rest-basic/v1
         var app = angular.module("ngTests", []);
-        console.log('asd2');
 
         app.controller('MainController', ['$http', function($http) {
-          console.log('inside controller');
 
         }]);
       })();
